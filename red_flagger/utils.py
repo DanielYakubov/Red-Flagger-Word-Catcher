69b86dd3cf40e744604890c24bd749e98a98f46c
Sara Davis <sarad@nevada.unr.edu>
2025-04-24 14:20:01 -0700

    replacing 239482 loops with sort and regex in filter_overlaps_and_sort and updating unit tests

--- red_flagger/utils.py
+++ red_flagger/utils.py
@@ -1,58 +1,53 @@
 """Utility functions for red flagger"""
 
+import re
+
 
 def filter_overlaps_and_sort(word_list: list[str]) -> list[str]:
     """Filters the word list to ensure items are unique within the word list.
 
     Filtering happens in three steps:
-    1) All the single gram items are extracted into a list, iff they are unique to the list. Multi-words are stored.
-    2) Multi-words are composed into strings. Each string is compared against every other string, and if the shorter string is
-        a sub-string of the longest, the longest is removed from the multi-word (not mult-string) container.
-    3) The multi-word container is iterated and added to the unique words list iff the n-grams (multi-words) do not contain existing n-1 grams.
+    1) All strings from word_list are broken into word lists, then all strings
+        are sorted by their word list length, and recombined into the original
+        phrase. This sorts the word_list (sorted_word_list) so that unigrams
+        are first, followed by bi, and so on.
+    2) Every word from the sorted_word_list is compared to every other word in
+        the list. If two phrases in the word_list are identical, the index of
+        the longer phrase is added to bad indices for later removal. If
+        phrase_b contains phrase_a, phrase_b is not unique and its index
+        is added to bad indices for later removal.
+    3) The bad_indices list from 2 is used to select only the unique phrases
+        in sorted_word_list
 
-    This algorithm also pseudo-sorts the list. First, all the unigrams are listed, then the multiword items.
+    This algorithm also pseudo-sorts the list. First, all the unigrams
+    are listed, then the multiword items.
     """
-    # NOT using a set here, want to be careful about determinism & insertion order.
-    unique_words: list[str] = []
-    multi_words: list[tuple[str, str, ...]] = []
-    multi_word_strings: list[str] = []
-
-    # gets unigrams, parses phrases (multi words) (compare lower case only: Cat == cat)
+    # Breaks up phrases into word lists, sorts list by length of word list,
+    # reforms phrase from word lists (now sorted)
     split_word_list = [x.split() for x in word_list]
-    unique_words = [
-        x[0].lower() for x in split_word_list
-        if len(x) == 1 and x[0].lower() not in unique_words
-    ]
-    multi_words = [
-        (y.lower() for y in x) for x in split_word_list # Changed this to a tuple since that is what the type hints were looking for
-        if len(x) > 1 and [y.lower() for y in x] not in multi_words # This last check looks like it exists to avoid duplicates? I would suggest just leaving it out and then removing the duplicates after, or using a set.
-    ]
+    split_word_list.sort(key=len)
+    sorted_word_list = [" ".join(word) for word in split_word_list]
 
-    #join tuples to form string for easier comparison of multi-word phrases
-   multi_word_strings = [" ".join(x).lower() for x in multi_words]
-
-    bad_indices = []
-
-    #compare every multi-word string to each other; if one is a substring of the other,append the longer phrase index to remove
-    [
-        bad_indices.append(i) for i in range(len(multi_word_strings))
-        for j in range(len(multi_word_strings))
-        if i != j and i not in bad_indices
-        and multi_word_strings[j] in multi_word_strings[i]
-    ]
-    #Use the bad indices from earlier comparison to remove phrases from multi-words that are covered by shorter phrase
-    #for example "a dog walks" would be removed if "a dog" was part of the same multiword set
-    multi_words = [
-        multi_word_tuple for i, multi_word_tuple in enumerate(multi_words)
+    # Compare every phrase from word list, only keep the unique words/phrases
+    bad_indices: list[int] = []
+    for i, phrase_a in enumerate(sorted_word_list):
+        for j, phrase_b in enumerate(sorted_word_list):
+            if i != j:
+                # handles dups in the word list (not caught by elif regex)
+                if phrase_a == phrase_b:
+                    if i not in bad_indices and j not in bad_indices:
+                        bad_indices.append(j)
+                elif (
+                    re.search(
+                        rf"\b{phrase_a}\b", phrase_b, flags=re.IGNORECASE
+                    )
+                    is not None
+                    and j not in bad_indices
+                ):
+                    bad_indices.append(j)
+    unique_wordlist = [
+        phrase
+        for i, phrase in enumerate(sorted_word_list)
         if i not in bad_indices
     ]
-    # checks unigrams in unique against every word in each phrase, stores only phrases with no uni-gram match
-    #for example "a dog" would be removed if "dog" was part of the unique word set
-    good_multi = [  #checks unigram (unique words) against multi words
-        multi_word_tuple for multi_word_tuple in multi_words
-        if all([word not in unique_words for word in multi_word_tuple])
-    ]
-
-    [unique_words.append(" ".join(x)) for x in good_multi]
-
-    return unique_words
+    return unique_wordlist
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-"""Tests various properties of the util functions which are critical to the lib."""
+"""Tests the util functions which are critical to the lib."""
 
 import unittest
 
@@ -9,17 +9,33 @@ class TestUtils(unittest.TestCase):
 
     def setUp(self):
         self.wordlist = [
-            "cat", "cats", "a cat", "a cat catches mice",
-            "The groom waited at the altar", "Cats groom", "dog",
-            "The Cat Napped", "the Cat caught a mouse", "a dog chased the bus",
-            "the Dog caught the cat", "the cat",
-            "The groom waited at the altar"
+            "a cat",
+            "a cat catches mice",
+            "The groom waited at the altar",
+            "Cats groom",
+            "dog",
+            "The Cat Napped",
+            "the Cat caught a mouse",
+            "a dog chased the bus",
+            "the Dog caught the cat",
+            "the cat",
+            "The groom waited at the altar",
+            "cat",
+            "cats",
+            "black Squirrel",
+            "black squirrels",
         ]
 
     def test_filter_overlaps_and_sort(self):
-        """Cat,dog,and cats are unigrams so any multigram containing them should be filtered out. Duplicates also filtered"""
+        """Cat,dog,and cats are unigrams so any multigram containing them
+        should be filtered out. Duplicates also filtered"""
         expected_result = [
-            "cat", "cats", "dog", "the groom waited at the altar"
+            "dog",
+            "cat",
+            "cats",
+            "black Squirrel",
+            "black squirrels",
+            "The groom waited at the altar",
         ]
         actual_result = filter_overlaps_and_sort(self.wordlist)
         self.assertEqual(expected_result, actual_result)
